7e9de81e2d1aeefdf330285d505cb09c275a3692
Kevin Matos <mat18015@byui.edu>
2020-02-08 17:45:11 -0700

    getting database stuff

--- web/index.php
+++ web/index.php
@@ -1,5 +1,31 @@
 <?php
 
-  phpinfo();
-
+  require "dbConnect.php";
+  $db = get_db();
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Teams</title>
+</head>
+
+<body>
+  <div>
+    <h1>Teams</h1>
+    
+    <?php
+      $statement = $db->prepare("SELECT * FROM team");
+      $statement->execute();
+
+      while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+      {
+        $id = $row['team_id'];
+        $name = $row['team_name'];
+        $winCount = $row['wins'];
+        $lossCount = $row['losses'];
+        echo "<p><strong>$name: $winCount - $lossCount</strong></p>";
+      }
+    ?>
+
+  </div>
+</body>
